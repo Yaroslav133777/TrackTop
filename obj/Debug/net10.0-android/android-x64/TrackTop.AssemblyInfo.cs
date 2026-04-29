96d15232e79800b7a3b09e5faa235f71468c4891
Yaroslav <pozitivnyjzmej@gmail.com>
2026-04-29 23:04:24 +0700

    text color updated

--- obj/Debug/net10.0-android/android-x64/TrackTop.AssemblyInfo.cs
+++ obj/Debug/net10.0-android/android-x64/TrackTop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackTop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3d5e2e42de9c758923dcad3e35e00d2628c769a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a917df0156059bf9d2183702f0ba449b99835534")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackTop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackTop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
